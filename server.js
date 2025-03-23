d892cf9e5920cd2ed4bfeef4c736ecbd1ca35289
0m0hsam <omohsam81@gmail.com>
2025-03-23 08:03:35 +0100

    update

--- server.js
+++ server.js
@@ -3,26 +3,28 @@ const app = express();
 const mongodb = require("./DB/database");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 dotenv.config();
 
 const port = process.env.PORT || 8080; // Use environment variable for port
 
 // Middleware
+app.use(cors()); // Enable CORS for all routes
 app.use(bodyParser.json()); // Parse JSON bodies
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/", require("./routes"));
 
 // Error handling middleware
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send("Something broke!");
-// });
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 mongodb.initDb((err) => {
   if (err) {
